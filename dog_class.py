4ded92a234d7c26cc963e4941284f001076f2d24
EmmanuelPure0x1 <eadejobi@spartaglobal.com>
2020-11-05 10:21:16 +0000

    further text added to README

--- dog_class.py
+++ dog_class.py
@@ -1,48 +1,64 @@
-# Classes, objects and instantiation
-
-# How to create a class
-# Syntax: class name_of_class starting with capital letters
-# good naming convention is required.
-
-# Classes are a way top bring data functionality together
+# # Classes, objects and instantiation
+#
+# # How to create a class
+# # Syntax: class name_of_class starting with capital letters
+# # good naming convention is required.
+#
+# # Classes are a way top bring data functionality together
+#
+# class Dog:
+#
+#     animal_kind = "canin"
+#
+# # defining 'class variable'
+#     def bark(self):
+#         # self refers to current class - self says that this func belongs to dog class
+#         return "woof"
+#
+# # to call we need to instantiate/initialise
+# # In order to execute a class we have to create an object of this class
+#
+# # creating object of Dog
+# # Objects and instances are the same
+# # creating and instance of the dog class named batman
+#
+# batman = Dog()
+#
+# # prints memory location of instance
+# print(batman)
+#
+# # prints the batman instance with the class attribute
+# print(batman.animal_kind)
+#
+# # prints the class instance along with the class method/function
+# print(batman.bark())
+#
+# DMX = Dog()
+# print("DMX Barks:", DMX.bark())
+# print(DMX.animal_kind)
+#
+# # any changes made to one object (not super class) will not have any impact to parent class.
+#
+# # This is account of the DMX obj
+# DMX.animal_kind = "Rapper"
+# print(DMX.animal_kind)
+#
+# # difference
+# print("batman is:", batman.animal_kind)
 
 class Dog:
 
-    animal_kind = "canin"
+    def __init__(self, name, colour): # initialising Dog Class
+        self.name = name
+        self.colour = colour
+        self.animal_kind = "canin"
 
-# defining 'class variable'
     def bark(self):
-        # self refers to current class - self says that this func belongs to dog class
         return "woof"
 
-# to call we need to instantiate/initialise
-# In order to execute a class we have to create an object of this class
+fido = Dog("fido", "brown") # creating an object of Dog Class
 
-# creating object of Dog
-# Objects and instances are the same
-# creating and instance of the dog class named batman
-
-batman = Dog()
-
-# prints memory location of instance
-print(batman)
-
-# prints the batman instance with the class attribute
-print(batman.animal_kind)
-
-# prints the class instance along with the class method/function
-print(batman.bark())
-
-DMX = Dog()
-print("DMX Barks:", DMX.bark())
-print(DMX.animal_kind)
-
-# any changes made to one object (not super class) will not have any impact to parent class.
-
-# This is account of the DMX obj
-DMX.animal_kind = "Rapper"
-print(DMX.animal_kind)
-
-# difference
-print("batman is:", batman.animal_kind)
-
+print(fido.name)
+print(fido.colour)
+print(fido.animal_kind)
+print(fido.bark()) #() after bark represents the fact that this is a method in the class.
